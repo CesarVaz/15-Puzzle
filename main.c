06f704a2529d77b10454fa716d9447ca22d5d233
CesarVaz <39285151+CesarVaz@users.noreply.github.com>
2018-10-30 11:56:32 -0300

    Add files via upload
    Proximos passos: 
    1. transferir pra uma arvore
    2. trabalhar no loop infinito
    3. pegar a distancia mais curta pra solução

--- main.c
+++ main.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
-#include "MyFunctions.h"
 #include <time.h>
-
+#include "Jogo.h"
+#include "ListaDuplaEstado.h"
+#include "Solver.h"
 int main(void)
 {
 	srand(time(NULL));
 	int matriz[4][4];
+	ListaDuplaEstado *lista = NULL;
 
 	preencheMatriz(matriz);
+	/*int matriz[4][4] = { {1,2,3,4}, {5,6,0,8}, {9,10,7,11}, {13,14,15,12} };*/
+
 	ImprimeJogo(matriz);
-
-	Move(matriz);
-
+	printf("MANHATAAAN  %d", ManhattanDistance(matriz));
 	_getch();
+
+	melhorCaminho(matriz, &lista);
+	
+	system("cls");
+	ImprimeJogo(matriz);
+	printf("MANHATAAAN  %d", ManhattanDistance(matriz));
+	printf("\ntamanho da lista: %d", tamanho(lista));
+	_getch();
+
     return 0;
 }
